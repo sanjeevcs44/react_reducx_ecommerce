f26717bb176f4c2aff60eccdc5817cc18b0d782d
Sanjeev Sharma <sanjeevcs44@gmail.com>
2021-11-10 20:44:05 +0530

    second commit

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from 'react';
 import axios from 'axios'
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setProducts } from '../redux/actions/productAction'
 import ProductComponent from './ProductComponent';
 
 const ProductListing = () => {
-    const products = useSelector((state) => state)
+
     const dispatch = useDispatch();
     const fetchProducts = async () => {
         const response = await axios.get("https://fakestoreapi.com/products").catch((err) => {
