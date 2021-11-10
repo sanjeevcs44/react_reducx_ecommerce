d7bdf6aef48c1cac8b889b27fd104753f289e567
Sanjeev Sharma <sanjeevcs44@gmail.com>
2021-11-10 20:15:28 +0530

    ecommerce with react and redux

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import myLogo from '../img/logo.jpeg';
+const Header = () => {
+    return (
+        <div className="ui fixed menu">
+            <div className="ui container center">
+                <h2>PrachiCraft</h2>
+            </div>
+        </div>
+    )
+}
+export default Header;
